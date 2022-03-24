c038ed8fe5fdc15624c72e67fa7143aaf373b952
jaskelly7 <102290283+jaskelly7@users.noreply.github.com>
2022-03-24 11:10:32 +0000

    Update myvm.tf

--- myvm.tf
+++ myvm.tf
@@ -3,11 +3,11 @@ provider "azurerm" {
   features {}
 }
 resource "azurerm_resource_group" "example" {
-  name     = "example"
+  name     = "examplejkrg"
   location = "West Europe"
 }
 
 resource "azurerm_resource_group" "example1" {
-  name     = "example1"
+  name     = "examplejkrf1"
   location = "West US"
 }
