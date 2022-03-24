92379ee8b3b3381910ab6d6da720bf35cb2ff320
jaskelly7 <102290283+jaskelly7@users.noreply.github.com>
2022-03-24 10:36:21 +0000

    Update myvm.tf

--- myvm.tf
+++ myvm.tf
@@ -6,3 +6,8 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "example1" {
+  name     = "example1"
+  location = "West US"
+}
